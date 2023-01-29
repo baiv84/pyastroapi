6dae46b7b1bfa2125667a2c5a0378c179f3df7c3
baiv84 <iambabintsev@gmail.com>
2023-01-29 20:25:19 +0700

    Step 2 - add feature to save file into particular folder

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
+import pathlib
 import requests
 from dotenv import load_dotenv
 
 
 IMAGE_URLS = [
     ('https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg', 'hubble.jpeg'),
+    ('https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg', 'hubble2.jpeg'),
 ]
 
 
-def download_image(url, filename):
-    """Download image via URL link"""
+def download_image(url, folder, filename):
+    """Download image via URL link to the folder"""
+    pathlib.Path(folder).mkdir(parents=True, exist_ok=True)
     response = requests.get(url)
     response.raise_for_status()
 
-    with open(filename, 'wb') as file:
+    output = f'{folder}/{filename}'
+    with open(output, 'wb') as file:
         file.write(response.content)
 
 
 if __name__ == '__main__':
     for url, fname in IMAGE_URLS:
-        download_image(url, fname)
+        download_image(url, 'images', fname)
