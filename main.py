d3632d729a86fc547a10be15b5bf7d8e514900f6
baiv84 <iambabintsev@gmail.com>
2023-01-29 20:11:22 +0700

    Step 1 - add function to download images via URL

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 import requests
 from dotenv import load_dotenv
 
+
+IMAGE_URLS = [
+    ('https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg', 'hubble.jpeg'),
+]
+
+
+def download_image(url, filename):
+    """Download image via URL link"""
+    response = requests.get(url)
+    response.raise_for_status()
+
+    with open(filename, 'wb') as file:
+        file.write(response.content)
+
+
 if __name__ == '__main__':
-    print('Astronomy lab...')
+    for url, fname in IMAGE_URLS:
+        download_image(url, fname)
