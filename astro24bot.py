3cdbbd834a77bb478749c4b145cf1ffa654309d9
baiv84 <iambabintsev@gmail.com>
2023-02-05 17:07:59 +0700

    Step 19 -add automatic photos sending to telegram

--- astro24bot.py
+++ astro24bot.py
@@ -2,10 +2,12 @@ import os
 import telegram
 from dotenv import load_dotenv
 
-def send_photo_to_channel(photo, text):
+
+def send_photo_to_channel(photo, text=None):
     """Send photo with message to channel"""
     load_dotenv()
     telegram_token = os.environ['TELEGRAM_TOKEN']
     bot = telegram.Bot(token=telegram_token)
-    bot.send_message(chat_id='@astro24channel', text=text)
+    if text:
+        bot.send_message(chat_id='@astro24channel', text=text)
     bot.send_document(chat_id='@astro24channel', document=open(photo, 'rb'))
---
+++ astronomy.py
@@ -0,0 +1,59 @@
+import os
+import time
+import random
+import argparse
+from dotenv import load_dotenv
+from astro24bot import send_photo_to_channel
+
+
+load_dotenv()
+
+
+INIT_COMMANDS = [
+    'python3 fetch_spacex_images.py',
+    'python3 fetch_spacex_images.py --launch_id=5eb87ce6ffd86e000604b33a',
+    'python3 fetch_nasa_images.py',
+    'python3 fetch_nasa_images.py --count 10',
+    'python3 fetch_epic_images.py',
+]
+
+
+IMAGES_FOLDER = os.environ['IMAGE_FOLDER']
+
+
+PHOTOS = []
+
+
+parser = argparse.ArgumentParser(
+    description='Space photo grabber'
+)
+parser.add_argument('--period', help='Delay period in hours')
+args = parser.parse_args()
+
+if not args.period:
+    print('Use default time delay')
+    UPDATE_PERIOD = int(os.environ['UPDATE_PERIOD']) * 3600
+else:
+    print('Use custom time delay')
+    UPDATE_PERIOD = int(args.period) * 3600
+
+
+def initialize_image_folder():
+    """Initialize images folder"""
+    global PHOTOS
+    global INIT_COMMANDS
+
+    for cmd in INIT_COMMANDS:
+        os.system(cmd)
+    PHOTOS = [f'images/{fname}' for fname in os.listdir(IMAGES_FOLDER)]
+
+
+while True:
+    if len(PHOTOS) == 0:
+        print("Initialize photo folder...")
+        initialize_image_folder()
+    else:
+        random.shuffle(PHOTOS)
+        photo = PHOTOS.pop()
+        send_photo_to_channel(photo)
+        time.sleep(UPDATE_PERIOD)
